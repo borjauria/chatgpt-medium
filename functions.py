558345b3cdbfb64b1434610655d99f30ba53a7b1
borjauria <borja@borjauria.es>
2023-09-04 13:37:21 +0200

    update docstring from read_config on functions.py

--- functions.py
+++ functions.py
@@ -2,6 +2,15 @@ import configparser
 
 # Method to read config file settings
 def read_config():
+    """
+    Loads and parses the configuration data from 'config.ini'.
+
+    This function reads the 'config.ini' file located in the current working directory
+    and returns a configuration object containing the sections and their respective settings.
+
+    Returns:
+        config: A configuration object containing data from 'config.ini'.
+    """
     config = configparser.ConfigParser()
     config.read('config.ini')
     return config
